4247a48b8fe59f089bf5854cc34406c47750de15
Wesley Sheng <wesley.sheng@microchip.com>
2019-03-05 21:06:00 +0800

    perror function

---
+++ perror.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+int main()
+{
+	errno = 0;
+	perror("errno = 0");	
+
+	errno = 1;
+	perror("errno = 1");	
+
+	errno = 2;
+	perror("errno = 2");	
+
+	errno = 3;
+	perror("errno = 3");	
+
+	errno = ENXIO;
+	printf("ENXIO %d : %d\n", errno, ENXIO);
+	perror("errno = ENXIO");	
+
+	
+	errno = 6;
+	perror("errno = 6");	
+
+	errno = EBUSY;
+	printf("EBUSY %d : %d\n", errno, EBUSY);
+	perror("errno = EBUSY");	
+
+	printf("+++ strerror ---\n");
+	errno = EBUSY;
+	printf("%s\n", strerror(errno));
+}
