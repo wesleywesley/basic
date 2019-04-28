6631e3f1624e8c63d5b166ed14290cc7490a4853
Wesley Sheng <wesley.sheng@microchip.com>
2019-04-28 22:31:33 +0800

    mmap for BAR2 NT
    1. loop cnt
    2. memcpy all shared memory

--- mmap.c
+++ mmap.c
@@ -1,13 +1,42 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/mman.h>
-#include <unistd.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+#define ARRAY_SIZE(array) (sizeof(array)/sizeof((array)[0]))
+
+struct shared_mw {
+	uint32_t magic;
+	uint32_t link_sta;
+	uint32_t partition_id;
+	uint64_t mw_size[128];
+	uint32_t spad[128];
+};
+
+int main(int argc, char *argv[])
 {
 	int fd;
 	void *map;
+	int ret;
+	int i, j;
+	unsigned long cnt;
+	struct shared_mw shared_mw;
+
+	for (i = 0; i < argc; i++)
+		fprintf(stdout, "argv[%d]:%s\n", i, argv[i]);
+
+	if (argc < 2) {
+		fprintf(stderr, "usage ./mmap <cnt>\n");
+		return -1;
+	}
+
+	cnt = strtol(argv[1], NULL, 0);
+	fprintf(stdout, "cnt: %ld\n", cnt);
 
 	fd = open("/sys/class/switchtec/switchtec0/device/resource2_wc", O_RDWR);
 	if (fd < 0) {
@@ -23,7 +52,17 @@ int main()
 		return -1; 
 	}
 
-	fprintf(stdout, "%x\n", *(uint32_t *)map);
+	for (i = 0; i < cnt; i++) {
+		memcpy(&shared_mw, map, sizeof(shared_mw));
+
+		fprintf(stdout, "magic %x\n", shared_mw.magic);
+		//fprintf(stdout, "link_sta %x\n", shared_mw.link_sta);
+		//fprintf(stdout, "partition %x\n", shared_mw.partition_id);
+
+		for (j = 0; j < ARRAY_SIZE(shared_mw.mw_size); j++) {
+			//fprintf(stdout, "mw_size[%d] %lx\n", j, shared_mw.mw_size[j]);
+		}
+	}
 
 	munmap(map, 4096);
 }
